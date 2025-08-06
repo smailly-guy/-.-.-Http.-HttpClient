ff9f155e796a33d2402c180a45c461b15523f429
smailly-guy <artline2010k@gmail.com>
2025-08-06 21:43:50 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8; // Підтримка українських символів у консолі
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string imageUrl = "https://raw.githubusercontent.com/github/explore/main/topics/csharp/csharp.png";
+        string base64 = await imageUrl.ToBase64ImageAsync();
+        Console.WriteLine(base64);
+
+
+        try
+        {
+            string base64Image = await imageUrl.ToBase64ImageAsync();
+            Console.WriteLine(base64Image); // Виведе Base64 у консоль
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+}
